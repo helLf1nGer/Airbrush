c04388350c80f77676159f42071a645588457655
WooshNavigation <97183810+WooshNavigation@users.noreply.github.com>
2023-02-14 11:55:35 -0500

    Clunky button logic implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,5 @@
 #include <Arduino.h>
 
-#include <Arduino.h>
-
 #define PUL 7
 #define DIR 6
 #define ENA 5
@@ -11,6 +9,16 @@ unsigned long button_start;
 unsigned long button_end;
 unsigned short pulseRev = 3200;
 unsigned short speed = 200;
+const unsigned short s1 = 100;
+const unsigned short s2 = 200;
+const unsigned short s3 = 300;
+const unsigned short s4 = 400;
+const unsigned short s5 = 500;
+unsigned int counter = 0;
+unsigned int rotation = 1;
+unsigned long last_push = 0;
+unsigned long timer_start = 0;
+unsigned short cutoff_time = 1000;
 
 void setup() {
   pinMode(PUL, OUTPUT);
@@ -21,35 +29,114 @@ void setup() {
   digitalWrite(PUL, LOW);
   digitalWrite(DIR, LOW);
   digitalWrite(ENA, HIGH);
+  Serial.begin(115200);
 }
 
 void rotateMotor() {
   digitalWrite(ENA, LOW);
+  digitalWrite(DIR, rotation == 1 ? LOW : HIGH);
   for (int i = 0; i < pulseRev; i++) {
     digitalWrite(PUL, HIGH);
     digitalWrite(PUL, LOW);
     delayMicroseconds(speed);
   }
   digitalWrite(ENA, HIGH);
-  }
+}
 
 void checkLongPress() {
   button_start = millis();
-
-  // Wait until the pushbutton is released
   while (digitalRead(BUTTON) == LOW) {
-    // Check if the pushbutton has been pushed for more than 300 milliseconds
     button_end = millis();
     if (button_end - button_start >= 300) {
-      // Invoke the rotateMotor function
       rotateMotor();
     }
   }
 }
 
-void loop() {
-  // Check if the pushbutton is being pushed
+void checkButtonPress() {
   if (digitalRead(BUTTON) == LOW) {
-    checkLongPress();
+    unsigned long current_time = millis();
+    if (counter == 0) {
+      button_start = current_time;
+      while (digitalRead(BUTTON) == LOW) {
+        button_end = millis();
+        if (button_end - button_start >= 300) {
+          rotateMotor();
+          // return;
+          counter = 0;
+        }
+      }
+      counter++;
+      last_push = current_time;
+      timer_start = current_time;
+    } else if (counter == 1) {
+      if (current_time - last_push >= cutoff_time) {
+        // rotateMotor();
+        counter = 0;
+        return;
+      } else {
+        button_start = current_time;
+        while (digitalRead(BUTTON) == LOW) {
+          button_end = millis();
+          if (button_end - button_start >= 300) {
+            rotateMotor();
+            counter = 0;
+            // return;
+          }
+        }
+        counter++;
+        last_push = current_time;
+        timer_start = current_time;
+      }
+    } else if (counter == 2) {
+      if (current_time - last_push >= cutoff_time) {
+        rotation = -rotation;
+        // rotateMotor();
+        counter = 0;
+        return;
+      } else {
+        button_start = current_time;
+        while (digitalRead(BUTTON) == LOW) {
+          button_end = millis();
+          if (button_end - button_start >= 300) {
+            rotateMotor();
+            counter = 0;
+            // return;
+          }
+        }
+        counter++;
+        last_push = current_time;
+        timer_start = current_time;
+      }
+    } else if (counter == 3) {
+      if (current_time - last_push >= cutoff_time) {
+        counter = 0;
+        return;
+      } else {
+        unsigned long timer_elapsed = current_time - timer_start;
+        if (timer_elapsed >= 6000) {
+          counter = 0;
+          return;
+        } else {
+          if (timer_elapsed >= 50 && timer_elapsed < 1000) {
+            speed = s1;
+          } else if (timer_elapsed >= 1000 && timer_elapsed < 2000) {
+            speed = s2;
+          } else if (timer_elapsed >= 2000 && timer_elapsed < 3000) {
+            speed = s3;
+          } else if (timer_elapsed >= 3000 && timer_elapsed < 4000) {
+            speed = s4;
+          } else if (timer_elapsed >= 4000 && timer_elapsed < 6000) {
+            speed = s5;
+          }
+        }
+      }
+    }
   }
+}
+
+void loop() {
+  checkButtonPress();
+  // checkLongPress();
+  Serial.println(counter);
 }
